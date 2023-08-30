097fe7e67dbd5c6776701f5df918e6f214cc9d29
Ryan Erkal <ryanerkal@yahoo.ie>
2023-08-30 05:53:21 +0100

    customers

--- src/pages/Balances.js
+++ src/pages/Balances.js
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function Balances() {
-	return <div>Balances</div>;
+	return (
+		<>
+			<h1 class="text-3xl font-bold m-4 p-4">Balances</h1>
+			<div class="m-4 grid grid-cols-2">
+				<div>
+					<div class="m-4">
+						<p class="text-l font-bold">Available Balance</p>
+						<p class="text-2xl font-bold">£0.00</p>
+					</div>
+					<div class="m-4">
+						<p class="text-l font-bold">Pending Balance</p>
+						<p class="text-2xl font-bold">£423.00</p>
+					</div>
+					<div class="m-4">
+						<p class="text-l font-bold">Total Balance</p>
+						<p class="text-2xl font-bold">£5343.00</p>
+					</div>
+				</div>
+				<div>
+					<div class="m-4">
+						<p class="text-l font-bold">Available Payouts</p>
+						<p class="text-2xl font-bold">£323.00</p>
+					</div>
+					<div class="m-4">
+						<p class="text-l font-bold">Pending Payouts</p>
+						<p class="text-2xl font-bold">£4134.00</p>
+					</div>
+					<div class="m-4">
+						<p class="text-l font-bold">Total Payouts</p>
+						<p class="text-2xl font-bold">£32325.00</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
